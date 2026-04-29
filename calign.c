864d19e056d91bec58498e9760937020389440f4
dakota-kosiorek <dakota.michael.kosiorek@gmail.com>
2026-04-29 16:06:18 -0400

    Started work in updating the traceback

--- calign.c
+++ calign.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 
+#define ERR 1
+#define TRUE 1
+#define FALSE 0
+
+int get_cell(int *m, int rows, int r, int c) {
+	return m[r * (rows + 1) + c];
+}
+
 int max(int a, int b, int c) {
 	int m_abc, m_bc;
 
@@ -17,98 +25,35 @@ int max(int a, int b, int c) {
 	return m_abc;
 }
 
-int main(int argc, char *argv[]) {
-	int gap = -1;
-	int mismatch = -1;
-	int match = 1;
-	int score = 0;
-
-	int i, j;
-	int d, h, v;
-
-	const char *valid_c = "ATGC";
-
-	if (argc != 3) {
-		printf("Too few arguments, './calign [SEQ 1] [SEQ 2]'\n");
-		return -1;
-	}
-
-	char *s1 = argv[1];
-	char *s2 = argv[2];
-
-	int s1_size = strlen(s1);
-	int s2_size = strlen(s2);
-
-	for (i = 0; i < s1_size; i++) s1[i] = toupper(s1[i]);
-	for (i = 0; i < s2_size; i++) s2[i] = toupper(s2[i]);
-
-	if (strspn(s1, valid_c) != s1_size || strspn(s2, valid_c) != s2_size) {
-		printf("Sequences must have valid characters: 'A|T|G|C'\n");
-		return -1;
-	}
-
-	// Allocated space for alignment; 1D array to be treated as a 2D array
-	int *matrix = malloc( ((s1_size + 1) * (s2_size + 1)) * sizeof(int) );
-	if (matrix == NULL) {
-		printf("Memory allocation error\n");
-		return -1;
-	}
-
-	// Initial 1st row and column values
-	for (i = 0; i < s1_size + 1; i++) matrix[i] = i * gap;
-	for (i = 0; i < s2_size + 1; i++) matrix[(s1_size + 1) * i] = i * gap;
-
-	// Alignment algorithm
-	for (i = 1; i < (s2_size + 1); i++) {
-		for (j = 1; j < (s1_size + 1); j++) {
-			d = matrix[(i - 1) * (s1_size + 1) + (j - 1)]; 	// Diagonal score
-			if (s1[j - 1] == s2[i - 1]) d += match;
-			else d += mismatch;
-
-			h = gap + matrix[i * (s1_size + 1) + (j - 1)];	// Horizontal score
-			v = gap + matrix[(i - 1) * (s1_size + 1) + j];	// Vertical score
-
-			matrix[i * (s1_size + 1) + j] = max(d, h, v);
-		}
-	}
-
-	// Print matrix
-	for (i = 0; i < (s2_size + 2); i++) {
-		for (j = 0; j < (s1_size + 2); j++) {
-			if (i == 0 && j == 0) 		printf("  ");
-			else if (i == 0 && j == 1)	printf("%3c ", ' ');
-			else if (i == 1 && j == 0)	printf("  ");
-			else if (i == 0 && j > 1) 	printf("%3c ", s1[j - 2]);
-			else if (i > 1 && j == 0) 	printf("%c ", s2[i - 2]);
-			else if (i > 0 && j > 0) 	printf("%3d ", matrix[(i-1) * (s1_size + 1) + (j-1)]);
-		} printf("\n");
-	}
-
-	// Traceback
-	printf("\n");
-
+int traceback(char *algn_code, int algn_size, int algn_max_size, 
+		int s1_size, int s2_size, 
+		int *m) {
 	// Allocate space for the alignment 'code'
-	int a_code_size = 0;
-	int a_code_max_size = s1_size + s2_size;
-	char *a_code = malloc( (a_code_max_size + 1) * sizeof(char));
-	if (matrix == NULL) {
-		printf("Memory allocation error\n");
-		free(matrix);
-		return -1;
+	if (algn_code == NULL) {
+		algn_size = 0;
+		algn_max_size = s1_size + s2_size;
+		algn_code = malloc( (algn_max_size + 1) * sizeof(char));
+		if (algn_code == NULL) {
+			printf("Memory allocation error\n");
+			return ERR;
+		}
+
+		for (int i = 0; i < algn_max_size; i++) algn_code[i] = 'E';
+		algn_code[algn_max_size] = '\0';
 	}
 
-	for (int i = 0; i < a_code_max_size; i++) a_code[i] = 'E';
-	a_code[a_code_max_size] = '\0';
-
-	i = s2_size;
-	j = s1_size;
+	// while row > 0 and column > 0
+	int i = s2_size;
+	int j = s1_size;
 
 	// Get the alignment code
 	while (i > 0 && j > 0) {
-		d = matrix[(i - 1)	* (s1_size + 1) + (j - 1)]; 	// Diagonal score
-		h = matrix[i		* (s1_size + 1) + (j - 1)];	// Horizontal score
-		v = matrix[(i - 1)	* (s1_size + 1) + j];		// Vertical score
-
+		int d = get_cell(m, s1_size, i - 1, j - 1);	// Diagonal score
+		int h = get_cell(m, s1_size, i    , j - 1);	// Horizontal score
+		int v = get_cell(m, s1_size, i - 1, j);	   	// Vertical score
+		printf("D: %d, H: %d, V: %d\n", d, h, v);
+		break;
+		/*
 		char c;
 		int dir = max(d, h, v);
 
@@ -140,70 +85,92 @@ int main(int argc, char *argv[]) {
 
 		a_code[a_code_size] = c;
 		a_code_size++;
+		*/
 	}
 
-	a_code[a_code_size] = '\0';
-	char *temp = realloc(a_code, a_code_size + 1);
-	if (temp == NULL) {
+	// start at bottom right of the matrix
+	// see if the left hoizontal, vetical, or left diagonal values are higher
+	// IF none of those values match, go to the higher cell and continue the loop
+	// ELSE rerun traceback starting at that cell with the initial alignment code done so far
+	// Print alignment
+	
+	free(algn_code);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	int gap = -1;
+	int mismatch = -1;
+	int match = 1;
+	int score = 0;
+
+	int d, h, v;
+
+	const char *valid_c = "ATGC";
+
+	if (argc != 3) {
+		printf("Too few arguments, './calign [SEQ 1] [SEQ 2]'\n");
+		return ERR;
+	}
+
+	char *s1 = argv[1];
+	char *s2 = argv[2];
+
+	int s1_size = strlen(s1);
+	int s2_size = strlen(s2);
+
+	for (int i = 0; i < s1_size; i++) s1[i] = toupper(s1[i]);
+	for (int i = 0; i < s2_size; i++) s2[i] = toupper(s2[i]);
+
+	if (strspn(s1, valid_c) != s1_size || strspn(s2, valid_c) != s2_size) {
+		printf("Sequences must have valid characters: 'A|T|G|C'\n");
+		return ERR;
+	}
+
+	// Allocated space for alignment; 1D int array to be treated as a 2D matrix
+	int *matrix = malloc( ((s1_size + 1) * (s2_size + 1)) * sizeof(int) );
+	if (matrix == NULL) {
 		printf("Memory allocation error\n");
-		free(matrix);
-		free(a_code);
-		return -1;
-	}
-	a_code = temp;
-
-	// Reverse alignment code
-	char *a_code_rev = malloc( (a_code_size + 1) * sizeof(char) );
-	if (a_code_rev == NULL) {
-		printf("Memory allocation error\n");
-		free(matrix);
-		free(a_code);
-		return -1;
-	}
-	a_code_rev[a_code_size] = '\0';
-
-	for (i = 0; i < a_code_size; i++) {
-		a_code_rev[i] = a_code[a_code_size - i - 1];
+		return ERR;
 	}
 
-	free(a_code);
+	// Initial 1st row and column values
+	for (int i = 0; i < s1_size + 1; i++) matrix[i] = i * gap;
+	for (int i = 0; i < s2_size + 1; i++) matrix[(s1_size + 1) * i] = i * gap;
 
-	// Print seq 1 alignment
+	// Alignment algorithm
+	for (int i = 1; i < (s2_size + 1); i++) {
+		for (int j = 1; j < (s1_size + 1); j++) {
+			d = matrix[(i - 1) * (s1_size + 1) + (j - 1)]; 	// Diagonal score
+			if (s1[j - 1] == s2[i - 1]) d += match;
+			else d += mismatch;
 
-	int offset = 0;
-	printf("Score %d\n", score);
+			h = gap + matrix[i * (s1_size + 1) + (j - 1)];	// Horizontal score
+			v = gap + matrix[(i - 1) * (s1_size + 1) + j];	// Vertical score
 
-	for (i = 0; i < a_code_size; i++) {
-		char c = a_code_rev[i];
-
-		if (c == 'D' || c == 'H')
-			c = s1[i - offset];
-		else if (c == 'V') {
-			c = '-';
-			offset++;
+			matrix[i * (s1_size + 1) + j] = max(d, h, v);
 		}
+	}
 
-		printf("%c", c);
-	} printf("\n");
+	// Print matrix
+	for (int i = 0; i < (s2_size + 2); i++) {
+		for (int j = 0; j < (s1_size + 2); j++) {
+			if (i == 0 && j == 0) 		printf("  ");
+			else if (i == 0 && j == 1)	printf("%3c ", ' ');
+			else if (i == 1 && j == 0)	printf("  ");
+			else if (i == 0 && j > 1) 	printf("%3c ", s1[j - 2]);
+			else if (i > 1 && j == 0) 	printf("%c ", s2[i - 2]);
+			else if (i > 0 && j > 0) 	printf("%3d ", matrix[(i-1) * (s1_size + 1) + (j-1)]);
+		} printf("\n");
+	}
 
-	offset = 0;
+	// Traceback
+	printf("\n");
 
-	// Print seq 2 alignment
-	for (i = 0; i < a_code_size; i++) {
-		char c = a_code_rev[i];
+	// TODO: fix the traceback so the BEST alignment(s) is shown
+	traceback(NULL, 0, 0, s1_size, s2_size, matrix);
+	// TODO: error handling where I free matrix and generated alignment code mem
 
-		if (c == 'D' || c == 'V')
-			c = s2[i - offset];
-		else if (c == 'H') {
-			c = '-';
-			offset++;
-		}
-
-		printf("%c", c);
-	} printf("\n");
-
-	free(a_code_rev);
 	free(matrix);
-
 	return 0;
 }
